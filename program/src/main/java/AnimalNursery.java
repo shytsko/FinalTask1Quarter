470ebc2877fcde539f6c1971fe07233905cd2958
Vladimir Shytsko <shytsko.v@gmail.com>
2023-02-23 13:13:17 +0300

    Add class AnimalNursery

---
+++ program/src/main/java/AnimalNursery.java
@@ -0,0 +1,42 @@
+import Animals.Animal;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class AnimalNursery implements Iterable {
+    Map<String, Animal> animals;
+
+    public AnimalNursery() {
+        this.animals = new HashMap<>();
+    }
+
+    public void addAnimal(Animal animal) {
+        this.animals.put(animal.getName(), animal);
+    }
+
+    public Animal getAnimal(String name) {
+        return this.animals.get(name);
+    }
+
+    public Animal removeAnimal(String name) {
+        return this.animals.remove(name);
+    }
+
+    public int getCount() {
+        return this.animals.size();
+    }
+
+    public boolean containsAnimal(String name) {
+        return this.animals.containsKey(name);
+    }
+
+    public boolean isEmpty() {
+        return this.animals.isEmpty();
+    }
+
+    @Override
+    public Iterator iterator() {
+        return this.animals.values().iterator();
+    }
+}
